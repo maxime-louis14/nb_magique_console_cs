2d0a7efa37b84f2a1f6918f91ded5c0707b83a82
maxime-l <maximelouis321@gmail.com>
2021-11-16 18:47:32 +0100

    Ajoutez des fichiers projet.

---
+++ nb_magique_console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nb_magique_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombreMagique = 5;
+            
+            while (true)
+            {
+
+                Console.WriteLine("devinez le nombre magique: ");
+                String reultat = Console.ReadLine();
+
+                int nombreUtilisateur = 0;
+                if (int.TryParse(reultat, out nombreUtilisateur)) {
+                    //La convertion s'est bien passée;
+
+                    if (nombreUtilisateur < nombreMagique) {
+                        Console.WriteLine("Le nombre magique est plus grand ");
+                    }
+                    else if(nombreUtilisateur > nombreMagique) {
+                        Console.WriteLine("Le nombre magique est plus petit. ");
+                    }
+                    else {
+                        //Egalité
+                        Console.WriteLine("BRAVO: Vous avez trouvé le nombre magique");
+                    }
+
+                } else {
+                    //Erreur de la conversion
+                    Console.WriteLine("ERREUR: vous devez rentre un nombre.");
+                  }
+
+           }
+        }
+    }
+}
